7c98f1dbdfc00b5d2a798833d9e2fd32de6f8e92
barrantes-sofiaanne <162861071+barrantes-sofiaanne@users.noreply.github.com>
2026-07-20 14:59:41 +0800

    Fix database connection error handling

--- PHP/dbcon.php
+++ PHP/dbcon.php
@@ -15,9 +15,7 @@ $conn = new mysqli(
 );
 
 if ($conn->connect_error) {
-    die("Database Connection Failed: " . $conn->connect_error);
+    die("Database Connection Failed: ".$conn->connect_error);
 }
 
 $conn->set_charset("utf8mb4");
-
-?>
